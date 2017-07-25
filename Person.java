c828021e02c1cb5a8b09f2bf19fab929688f7673
wj1996 <15236730688@163.com>
2017-07-25 14:37:38 +0800

    modify Person.java

--- Person.java
+++ Person.java
@@ -1,8 +1,10 @@
-public Person{
+public class Person{
 	private String name;
 	private int age;
 	
-	System.out.println("lalala");
-	System.out.println("hehehe");
-	System.out.println("hahaha");
+	public static void main(String...args){
+		System.out.println("lalala");
+		System.out.println("hehehe");
+		System.out.println("hahaha");
+	}
 }
